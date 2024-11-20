5690c504c3c5fe91c47fae1e00d712549e74f30a
RahulArora0603 <rahul.arora0603@gmail.com>
2024-11-20 21:04:53 +0530

    initial commit

--- Linear Regression/linear_from_scratch.py
+++ Linear Regression/linear_from_scratch.py
@@ -5,27 +5,27 @@ import pandas as pd
 class LinearRegression:
 
     def __init__(self, lr=0.0001, n_iters=1000):
-        self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
+        self.lr = lr   #learning rate
+        self.n_iters = n_iters #number of iterations
+        self.weights = None   #weights
+        self.bias = None      #bias
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+    def fit(self, X, y):         #fit method (training happens here)
+        n_samples, n_features = X.shape #n_samples --> number of rows, n_features --> number of cols
+        self.weights = np.zeros(n_features) #array of length n_features filled with zeros
+        self.bias = 0  
 
-        for _ in range(self.n_iters):
+        for _ in range(self.n_iters):  #loop
 
-            y_pred = np.dot(X, self.weights) + self.bias
+            y_pred = np.dot(X, self.weights) + self.bias  #dot product of data and weights + bias (mx+b)
     
-            dw = (1/n_samples)*np.dot(X.T, (y_pred-y))
-            db = (1/n_samples)*np.sum(y_pred-y)
+            dw = (1/n_samples)*np.dot(X.T, (y_pred-y)) #derivative of weights
+            db = (1/n_samples)*np.sum(y_pred-y)        #derivative of bias
     
-            self.weights = self.weights - self.lr * dw
-            self.bias = self.bias - self.lr * db 
+            self.weights = self.weights - self.lr * dw  #updating the weights using dw and learning rate
+            self.bias = self.bias - self.lr * db        # "    "      bias     " "  db and learning rate
 
     def predict(self, X):
-        y_pred = np.dot(X, self.weights) + self.bias
+        y_pred = np.dot(X, self.weights) + self.bias #final prediction
         return y_pred
         
--- Logistic Regression/logistic_reg.py
+++ Logistic Regression/logistic_reg.py
@@ -6,31 +6,31 @@ def sigmoid(x):
 class LogisticRegression():
 
     def __init__(self, lr=0.001, n_iters=1000):
-        self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
+        self.lr = lr   #learning rate
+        self.n_iters = n_iters #number of iterations
+        self.weights = None #weights
+        self.bias = None   #bias
 
     def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        n_samples, n_features = X.shape #n_samples - no. of rows, n_features - number of columns
+        self.weights = np.zeros(n_features) #array of length n_features where each weight is 0
+        self.bias = 0 
 
-        for _ in range(self.n_iters):
-            linear_pred = np.dot(X, self.weights) + self.bias
-            predictions = sigmoid(linear_pred)
+        for _ in range(self.n_iters): #loop
+            linear_pred = np.dot(X, self.weights) + self.bias #dot product of data and weights + bias
+            predictions = sigmoid(linear_pred) #apply sigmoid function to data
 
-            dw = (1/n_samples) * np.dot(X.T, (predictions - y))
-            db = (1/n_samples) * np.sum(predictions-y)
+            dw = (1/n_samples) * np.dot(X.T, (predictions - y)) #derivative of weights
+            db = (1/n_samples) * np.sum(predictions-y) #derivative of bias
 
-            self.weights = self.weights - self.lr*dw
-            self.bias = self.bias - self.lr*db
+            self.weights = self.weights - self.lr*dw #updating the weights
+            self.bias = self.bias - self.lr*db #updating the bias
 
 
     def predict(self, X):
-        linear_pred = np.dot(X, self.weights) + self.bias
-        y_pred = sigmoid(linear_pred)
-        class_pred = [0 if y<=0.5 else 1 for y in y_pred]
+        linear_pred = np.dot(X, self.weights) + self.bias #dot product based on final weights and bias
+        y_pred = sigmoid(linear_pred) #final prediction
+        class_pred = [0 if y<=0.5 else 1 for y in y_pred] #classifying
         return class_pred
 
     
